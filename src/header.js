fa2a1ee381765ddfdb022bad4e8c3592dea9dc0f
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-02 17:24:33 +0200

    Add info in the webpack config

---
+++ src/header.js
@@ -0,0 +1,9 @@
+export default function header() {
+  const mainContainer = document.createComment('header');
+  // const nav = document.createElement('nav');
+  // const ul = document.createElement('ul');
+  // const li = document.createElement('a');
+  // const logo = document.createElement('a')
+
+  mainContainer.classList = 'flex-between';
+}
