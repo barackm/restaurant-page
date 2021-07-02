493df0f04649f1b5318f137b7adefc893ba6badf
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-07-02 20:23:28 +0200

    Finish the javascript logic for creating the contact page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+import contactContainer from './contact';
 import header from './header';
 import homeContainer from './home';
 import initialize from './initialize';
 import menuContainer from './menu';
 
 initialize.append(header());
+
+const showHomeLink = document.querySelector('.home-link');
+const showMenuLink = document.querySelector('.menu-link');
+const showContactLink = document.querySelector('.contact-link');
+
+showContactLink.addEventListener('click', () => {});
+
+showHomeLink.addEventListener('click', () => {});
+
+showMenuLink.addEventListener('click', () => {});
+
 initialize.append(homeContainer());
 initialize.append(menuContainer());
+initialize.append(contactContainer());
